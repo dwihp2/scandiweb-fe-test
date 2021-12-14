e4280e006f729dd31bc2a68bda3eebecb6c93805
dwihp2 <m.dwiheryanto2@gmail.com>
2021-12-14 17:58:00 +0700

    add(page component): add new category pages

---
+++ src/components/Pages/men.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class MenPage extends Component {
+  render() {
+    return (
+      <>
+        <div className="category-name">
+          <h2>Men Page</h2>
+        </div>
+      </>
+    );
+  }
+}
+
+export default MenPage;
---
+++ src/components/Pages/women.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class WomenPage extends Component {
+  render() {
+    return (
+      <>
+        <div className="category-name">
+          <h2>Women Page</h2>
+        </div>
+      </>
+    );
+  }
+}
+
+export default WomenPage;

a9a52c9031eca3795429ceb88c4355b74be0b6d2
dwihp2 <m.dwiheryanto2@gmail.com>
2021-12-14 17:59:01 +0700

    fix: change font-family and configure class component with react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
 import Navbar from "./components/NavBar/NavBar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import { KidsPage, WomenPage, MenPage } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/women" element={<WomenPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/men" element={<MenPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/kids" element={<KidsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as NavBar } from "./NavBar/NavBar";
+export { default as KidsPage } from "./Pages/kids";
+export { default as WomenPage } from "./Pages/women";
+export { default as MenPage } from "./Pages/men";
